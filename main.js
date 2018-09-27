13e133a255638b58a2cf6f371767f22426913b3d
tPenguinLTG <tpenguinltg@disroot.org>
2018-09-26 20:34:13 -0400

    Add repl
    This allows ed to be run and tested in a terminal.

---
+++ main.js
@@ -0,0 +1,18 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const Ed = require("./src/ed");
+const ed = new Ed();
+const instance = ed.run();
+instance.next();
+
+readline.on("line", (line) => {
+  const iteration = instance.next(line);
+  if (iteration.done) {
+    process.exit(iteration.value);
+  } else {
+    process.stdout.write(iteration.value);
+  }
+});
