669d96bd2d9ae431fa18b842041ba5d78b2fb683
tPenguinLTG <tpenguinltg@disroot.org>
2018-10-02 18:46:08 -0400

    Indent with tabs instead of spaces
    Using tabs for indentation makes editing with ed(1) easier.

--- src/ed.js
+++ src/ed.js
@@ -1,32 +1,32 @@
 class Ed {
-  constructor({prompt} = {}) {
-    this.promptString = prompt || "*";
-    this.showPrompt = !!prompt;
-  }
+	constructor({prompt} = {}) {
+		this.promptString = prompt || "*";
+		this.showPrompt = !!prompt;
+	}
 
-  get prompt() {
-    return (this.showPrompt)? this.promptString : "";
-  }
+	get prompt() {
+		return (this.showPrompt)? this.promptString : "";
+	}
 
-  *run() {
-    let input;
-    let output = "";
+	*run() {
+		let input;
+		let output = "";
 
-    while (typeof (input = yield (output + this.prompt)) == "string") {
-      output = "";
-      switch (input) {
-        case "P":
-          this.showPrompt = !this.showPrompt;
-          break;
-        case "q":
-        case "Q":
-          return 0;
-        default:
-          output = "?\n";
-      }
-    }
-  }
+		while (typeof (input = yield (output + this.prompt)) == "string") {
+			output = "";
+			switch (input) {
+				case "P":
+					this.showPrompt = !this.showPrompt;
+					break;
+				case "q":
+				case "Q":
+					return 0;
+				default:
+					output = "?\n";
+			}
+		}
+	}
 }
 
 if (typeof module !== "undefined")
-  module.exports = Ed;
+	module.exports = Ed;
--- test/ed.test.js
+++ test/ed.test.js
@@ -2,110 +2,109 @@ const Ed = require("../src/ed");
 const assert = require("assert").strict;
 
 describe("Ed", () => {
-  let ed;
+	let ed;
 
-  beforeEach(() => {
-    ed = new Ed();
-  });
+	beforeEach(() => {
+		ed = new Ed();
+	});
 
-  it("should exit on non-string input, signalling EOF", () => {
-    const instance = ed.run();
+	it("should exit on non-string input, signalling EOF", () => {
+		const instance = ed.run();
 
-    instance.next();
-    const iteration = instance.next();
+		instance.next();
+		const iteration = instance.next();
 
-    assert.ok(iteration.done, "The generator should be done.");
-  });
+		assert.ok(iteration.done, "The generator should be done.");
+	});
 
-  describe("defaults", () => {
-    it("should set '*' as the prompt", () => {
-      assert.equal(ed.promptString, "*");
-    });
+	describe("defaults", () => {
+		it("should set '*' as the prompt", () => {
+			assert.equal(ed.promptString, "*");
+		});
 
-    it("should hide the prompt", () => {
-      assert.ok(!ed.showPrompt);
-    });
-  });
+		it("should hide the prompt", () => {
+			assert.ok(!ed.showPrompt);
+		});
+	});
 
-  describe("options", () => {
-    describe("prompt", () => {
-      it("should set prompt string", () => {
-        const prompt = ":";
+	describe("options", () => {
+		describe("prompt", () => {
+			it("should set prompt string", () => {
+				const prompt = ":";
 
-        ed = new Ed({prompt});
+				ed = new Ed({prompt});
 
-        assert.equal(ed.promptString, prompt);
-      });
+				assert.equal(ed.promptString, prompt);
+			});
 
-      it("should enable prompt", () => {
-        const prompt = ":";
+			it("should enable prompt", () => {
+				const prompt = ":";
 
-        ed = new Ed({prompt});
+				ed = new Ed({prompt});
 
-        assert.ok(ed.showPrompt);
-      });
+				assert.ok(ed.showPrompt);
+			});
 
-      it("should set default and disable if not specified", () => {
-        assert.equal(ed.promptString, "*");
-        assert.ok(!ed.showPrompt);
-      });
-    });
-  });
+			it("should set default and disable if not specified", () => {
+				assert.equal(ed.promptString, "*");
+				assert.ok(!ed.showPrompt);
+			});
+		});
+	});
 
-  describe("command", () => {
-    describe("unknown", () => {
-      it("should return '?' and wait for more input", () => {
-        const instance = ed.run();
-        instance.next();
+	describe("command", () => {
+		describe("unknown", () => {
+			it("should return '?' and wait for more input", () => {
+				const instance = ed.run();
+				instance.next();
 
-        const iteration = instance.next("notacommand");
+				const iteration = instance.next("notacommand");
 
-        assert.equal(iteration.value, "?\n");
-        assert.ok(!iteration.done);
-      });
-    });
+				assert.equal(iteration.value, "?\n");
+				assert.ok(!iteration.done);
+			});
 
-    describe("P", () => {
-      it("should show prompt if prompt is disabled", () => {
-        const instance = ed.run();
-        instance.next();
+		describe("P", () => {
+			it("should show prompt if prompt is disabled", () => {
+				const instance = ed.run();
+				instance.next();
 
-        const iteration = instance.next("P");
+				const iteration = instance.next("P");
 
-        assert.equal(iteration.value, ed.promptString);
-      });
+				assert.equal(iteration.value, ed.promptString);
+			});
 
-      it("should hide prompt if prompt is enabled", () => {
-        ed.showPrompt = true;
-        const instance = ed.run();
-        instance.next();
+			it("should hide prompt if prompt is enabled", () => {
+				ed.showPrompt = true;
+				const instance = ed.run();
+				instance.next();
 
-        const iteration = instance.next("P");
+				const iteration = instance.next("P");
 
-        assert.equal(iteration.value, "");
-      });
-    });
+				assert.equal(iteration.value, "");
+			});
+		});
 
-    describe("q", () => {
-      it("should quit", () => {
-        const instance = ed.run();
-        instance.next();
+		describe("q", () => {
+			it("should quit", () => {
+				const instance = ed.run();
+				instance.next();
 
-        const iteration = instance.next("q");
+				const iteration = instance.next("q");
 
-        assert.ok(iteration.done, "The generator should be done.");
-      });
-    });
+				assert.ok(iteration.done, "The generator should be done.");
+			});
+		});
 
-    describe("Q", () => {
-      it("should quit", () => {
-        const instance = ed.run();
-        instance.next();
+		describe("Q", () => {
+			it("should quit", () => {
+				const instance = ed.run();
+				instance.next();
 
-        const iteration = instance.next("Q");
+				const iteration = instance.next("Q");
 
-        assert.ok(iteration.done, "The generator should be done.");
-      });
-    });
-  });
+				assert.ok(iteration.done, "The generator should be done.");
+			});
+		});
+	});
 });
